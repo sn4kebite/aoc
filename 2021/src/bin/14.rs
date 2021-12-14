6aab7a9c17aed915ab45f6baf81cf891da35f2cf
Jon Bergli Heier <snakebite@jvnv.net>
2021-12-14 17:46:38 +0100

    2021/14: Add comments and some minor changes

--- 2021/src/bin/14.rs
+++ 2021/src/bin/14.rs
@@ -21,29 +21,34 @@ fn run_insertion(filename: &str, steps: usize) -> usize {
         let to = to.chars().nth(0).expect("to");
         rules.insert(from, to);
     });
-    let mut rule_mutations: HashMap<String, (String, String)> = HashMap::new();
+    // Map of rule results, eg."NN -> C" becomes ("NN", ("NC", "CN", 'C'))
+    // This is a simpler to use version of the parsed rules. The last tuple element is just to
+    // avoid having to look up the result element from one of the result pairs.
+    let mut rule_mutations: HashMap<String, (String, String, char)> = HashMap::new();
     for (from, to) in &rules {
         rule_mutations.insert(
             from.clone(),
             (
-                String::from(
-                    vec![&from.chars().nth(0).unwrap(), to]
-                        .into_iter()
-                        .collect::<String>(),
-                ),
-                String::from(
-                    vec![to, &from.chars().nth(1).unwrap()]
-                        .into_iter()
-                        .collect::<String>(),
-                ),
+                vec![&from.chars().nth(0).unwrap(), to]
+                    .into_iter()
+                    .collect::<String>(),
+                vec![to, &from.chars().nth(1).unwrap()]
+                    .into_iter()
+                    .collect::<String>(),
+                *to,
             ),
         );
     }
+    // Number of current instance per rule, modified based on rule_mutations
     let mut rule_count: HashMap<String, usize> = HashMap::new();
+    // Number of current instances per element, modified based on rule_count.
+    // Also used directly to calculate the final result.
     let mut element_count: HashMap<char, usize> = HashMap::new();
     for c in template.chars() {
         *element_count.entry(c).or_insert(0) += 1;
     }
+    // Fill rule_count based on the template. This loop is essentially a moving 2-character window
+    // along the template string.
     for i in 0..template.len() - 1 {
         for (from, _) in &rules {
             let from = from.clone();
@@ -53,16 +58,21 @@ fn run_insertion(filename: &str, steps: usize) -> usize {
         }
     }
     for _ in 0..steps {
+        // rule_count modifications to be done after all mutations are processed.
         let mut add_rules = vec![];
-        for (from, (a, b)) in &rule_mutations {
-            let e = rule_count.entry(from.to_string()).or_insert(0);
-            if *e > 0 {
-                add_rules.push((a.to_string(), *e));
-                add_rules.push((b.to_string(), *e));
-                *element_count.entry(b.chars().nth(0).unwrap()).or_insert(0) += *e;
-                *e = 0;
+        for (from, (a, b, c)) in &rule_mutations {
+            let rule = rule_count.entry(from.to_string()).or_insert(0);
+            // Non-zero if we have any instances of this rule in our polymer.
+            if *rule > 0 {
+                // New rule instances, to be updated afterwards
+                add_rules.push((a.to_string(), *rule));
+                add_rules.push((b.to_string(), *rule));
+                // Add the resulting element from a pair insertion based on the number of rule instances.
+                *element_count.entry(*c).or_insert(0) += *rule;
+                *rule = 0;
             }
         }
+        // Finally update the rule instance counts
         for (rule, count) in add_rules {
             *rule_count.entry(rule).or_insert(0) += count;
         }
