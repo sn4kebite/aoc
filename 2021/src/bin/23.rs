a50b89137d0cdff09747a79e16d208b7891974da
Jon Bergli Heier <snakebite@jvnv.net>
2022-03-27 21:23:16 +0200

    2021: Complete day 23

--- 2021/src/bin/23.rs
+++ 2021/src/bin/23.rs
@@ -4,70 +4,45 @@ use std::fs::File;
 use std::io::BufRead;
 use std::io::BufReader;
 
-fn run2(hallway: &[char; 11], rooms: &[char; 8]) -> usize {
+fn organize_amphipods(hallway: &[char; 11], rooms: &[char; 16], room_size: usize) -> usize {
     let mut states = HashSet::new();
     let mut queue = BinaryHeap::new();
-    for room in 0..4 {
-        let room_type = (b'A' + (room as u8)) as char;
-        if rooms[room * 2 + 0] == room_type && rooms[room * 2 + 1] == room_type {
-            continue;
-        }
-        if rooms[room * 2 + 0].is_alphabetic() {
-            let mut hallway = hallway.clone();
-            let mut rooms = rooms.clone();
-            let c = rooms[room * 2 + 0];
-            rooms[room * 2 + 0] = '.';
-            for i in [0, 1, 3, 5, 7, 9, 10] {
-                if hallway[i] == '.' {
-                    hallway[i] = c;
-                    queue.push(Reverse((0, hallway.clone(), rooms.clone())));
-                }
-            }
-        }
-    }
-    let mut lowest_cost = None;
+    let room_count = 4;
+    queue.push(Reverse((0, hallway.clone(), rooms.clone())));
     while let Some(Reverse((cost, hallway, rooms))) = queue.pop() {
-        //println!("{:?} {:?} {}", hallway, rooms, cost);
-        if rooms.iter().enumerate().all(|(room, c)| {
-            let room_type = (b'A' + (room as u8) / 2) as char;
-            *c == room_type
-        }) {
-            println!("done with cost {}", cost);
-            if lowest_cost.is_none() {
-                lowest_cost = Some(cost);
-                break;
-            } else if let Some(c) = lowest_cost {
-                if cost < c {
-                    lowest_cost = Some(cost);
-                }
-            }
-            continue;
+        if rooms[0..room_size * room_count]
+            .iter()
+            .enumerate()
+            .all(|(room, c)| {
+                let room_type = (b'A' + (room / room_size) as u8) as char;
+                *c == room_type
+            })
+        {
+            return cost;
         }
         if states.contains(&(hallway, rooms)) {
             continue;
         }
         states.insert((hallway.clone(), rooms.clone()));
-        for room in 0..4 {
+        for room in 0..room_count {
             let room_type = (b'A' + (room as u8)) as char;
-            if rooms[room * 2 + 0] == room_type && rooms[room * 2 + 1] == room_type {
+            if rooms[room * room_size..(room + 1) * room_size]
+                .iter()
+                .all(|c| *c == room_type)
+            {
                 continue;
             }
             // room position in the hallway
             let room_pos = 2 + room * 2;
-            for pos in 0..2 {
-                if !rooms[room * 2 + pos].is_alphabetic() {
+            // 0=outer, room_size=inner
+            for pos in 0..room_size {
+                if !rooms[room * room_size + pos].is_alphabetic() {
                     continue;
                 }
                 let mut rooms = rooms.clone();
-                let c = rooms[room * 2 + pos];
-                rooms[room * 2 + pos] = '.';
-                let mut next = vec![0, 1, 3, 5, 7, 9, 10];
-                next.sort_by_key(|v| *v as i32 - (2 + room as i32 * 2));
-                for i in next {
-                    let dir: i32 = if room_pos > i { -1 } else { 1 };
-                    if (dir == 1 && i == hallway.len() - 1) || (dir == -1 && i == 0) {
-                        continue;
-                    }
+                let c = rooms[room * room_size + pos];
+                rooms[room * room_size + pos] = '.';
+                for i in [0, 1, 3, 5, 7, 9, 10] {
                     let mut start = room_pos;
                     let mut end = i;
                     if start > end {
@@ -77,13 +52,13 @@ fn run2(hallway: &[char; 11], rooms: &[char; 8]) -> usize {
                     } else {
                         end = end + 1;
                     }
-                    if hallway[start..end].iter().all(|c| *c == '.') {
+                    if hallway[start..end].iter().all(|t| *t == '.') {
                         let mut hallway = hallway.clone();
                         hallway[i] = c;
                         let cost = cost
-                            + 10_usize.pow(room as u32)
-                                * ((i as i32 - room_pos as i32).abs() as usize + pos + 1);
-                        queue.push(Reverse((cost, hallway, rooms.clone())));
+                            + 10_usize.pow((c as u8 - b'A') as u32)
+                                * ((end - start - 1) as usize + pos + 1);
+                        queue.push(Reverse((cost, hallway, rooms)));
                     }
                 }
                 // we wan't move from the inner position if the outer position was occupied
@@ -98,16 +73,18 @@ fn run2(hallway: &[char; 11], rooms: &[char; 8]) -> usize {
             hallway[i] = '.';
             let room_type = *c;
             let room = (room_type as u8 - b'A') as usize;
-            for pos in 0..2 {
-                let pos = 1 - pos;
-                if rooms[room * 2 + pos] != '.' {
-                    continue;
+            for pos in 0..room_size {
+                // reverse so we start with the inner tile
+                let pos = room_size - 1 - pos;
+                // Skip rooms we can't move to
+                match rooms[room * room_size + pos] {
+                    //'A'..'D' if rooms[room * room_size + pos] != room_type => break,
+                    'A' | 'B' | 'C' | 'D' if rooms[room * room_size + pos] != room_type => break,
+                    'A' | 'B' | 'C' | 'D' if rooms[room * room_size + pos] == room_type => continue,
+                    '.' => (),
+                    _ => panic!("invalid tile value!"),
                 }
                 let room_pos = 2 + room * 2;
-                let dir: i32 = if room_pos < i { -1 } else { 1 };
-                if (dir == 1 && i == hallway.len() - 1) || (dir == -1 && i == 0) {
-                    continue;
-                }
                 let mut start = i;
                 let mut end = room_pos;
                 if start > end {
@@ -117,25 +94,23 @@ fn run2(hallway: &[char; 11], rooms: &[char; 8]) -> usize {
                 } else {
                     end = end + 1;
                 }
-                if hallway[start..end].iter().all(|c| *c == '.') {
+                if hallway[start..end].iter().all(|t| *t == '.') {
                     let mut rooms = rooms.clone();
-                    rooms[room * 2 + pos] = *c;
-                    let cost = cost
-                        + 10_usize.pow(room as u32)
-                            * ((i as i32 - room_pos as i32).abs() as usize + pos + 1);
-                    queue.push(Reverse((cost, hallway.clone(), rooms)));
-                    break;
+                    rooms[room * room_size + pos] = room_type;
+                    let cost =
+                        cost + 10_usize.pow(room as u32) * ((end - start - 1) as usize + pos + 1);
+                    queue.push(Reverse((cost, hallway, rooms)));
                 }
+                break;
             }
         }
     }
-    println!("unique states {}", states.len());
-    lowest_cost.unwrap()
+    0
 }
 
-fn run(filename: &str) -> (usize, usize) {
-    let mut hallway = ['.'; 11];
-    let mut rooms = ['.'; 8];
+fn run2(filename: &str, room_size: usize) -> usize {
+    let hallway = ['.'; 11];
+    let mut rooms = ['.'; 16];
     let file = File::open(filename).unwrap();
     let mut reader = BufReader::new(file);
     let mut buf = String::new();
@@ -148,15 +123,26 @@ fn run(filename: &str) -> (usize, usize) {
             let mut room = 0;
             for c in buf.chars() {
                 if c.is_alphabetic() {
-                    rooms[room * 2 + pos] = c;
+                    rooms[room * room_size + pos] = c;
                     room += 1;
                 }
             }
         }
+        if room_size == 4 {
+            // Mutate the room for the second part
+            let mut_room = b"DDCBBAAC";
+            for room in 0..4 {
+                rooms[room * room_size + 3] = rooms[room * room_size + 1];
+                rooms[room * room_size + 1] = mut_room[room * 2 + 0] as char;
+                rooms[room * room_size + 2] = mut_room[room * 2 + 1] as char;
+            }
+        }
     }
-    println!("hallway {:?}", hallway);
-    println!("rooms {:?}", rooms);
-    (run2(&hallway, &rooms), 0)
+    organize_amphipods(&hallway, &rooms, room_size)
+}
+
+fn run(filename: &str) -> (usize, usize) {
+    (run2(&filename, 2), run2(&filename, 4))
 }
 
 fn main() {
@@ -170,13 +156,13 @@ mod tests {
     fn test_example_23() {
         let (first, second) = super::run("input/23-example.txt");
         assert_eq!(first, 12521);
-        assert_eq!(second, 0);
+        assert_eq!(second, 44169);
     }
 
     #[test]
     fn test_input_23() {
         let (first, second) = super::run("input/23.txt");
-        assert_eq!(first, 0);
-        assert_eq!(second, 0);
+        assert_eq!(first, 14350);
+        assert_eq!(second, 49742);
     }
 }
